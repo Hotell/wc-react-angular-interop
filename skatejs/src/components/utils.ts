d009d3e99073b6a5b99b140956f9691455a2d9a7
Martin Hochel <hochelmartin@gmail.com>
2016-09-30 18:18:02 +0200

    feat(utils): add omit fn

--- skatejs/src/components/utils.ts
+++ skatejs/src/components/utils.ts
@@ -3,3 +3,14 @@ import { symbols } from 'skatejs';
 export function getShadowRoot( elem ) {
   return elem[ symbols.shadowRoot ];
 }
+
+export function omit( obj: {[key: string]: any}, excludeKeys: string[] ): {[key: string]: any} {
+  const objKeys = Object.keys( obj );
+  return objKeys
+    .reduce( ( acc, keyName ) => {
+      if ( !excludeKeys.includes( keyName ) ) {
+        acc[ keyName ] = obj[ keyName ]
+      }
+      return acc;
+    }, {} )
+}
